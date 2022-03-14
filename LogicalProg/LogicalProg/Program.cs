432c6e814421dcae45db283216ce57788d79c230
kiran1902 <kiru2kumar@gmail.com>
2022-03-14 22:41:17 +0530

    Fibonacci Series

--- LogicalProg/LogicalProg/Program.cs
+++ LogicalProg/LogicalProg/Program.cs
@@ -1,4 +1,29 @@
-﻿using LogicalProg;
+﻿namespace LogicalProg
+{
+    class Fibonacci
+    {
+        static void Main(String[] args)
+        {
+            //local variables
+            int a = 0;
+            int b = 1;
+            int c;
 
-Fibonacci myFibo = new Fibonacci();
-myFibo.fibonacci();
+            Console.WriteLine("Enter The Number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.Write(a + " " + b + " "); //printing 0 and 1
+
+            //for loop
+            for (int i = 0; i < number; i++)
+            {
+                c = a + b;
+                Console.WriteLine(c);
+
+                //swapping a and b
+                a = b;
+                b = c;
+            }
+            Console.ReadLine();
+        }
+    }
+}
