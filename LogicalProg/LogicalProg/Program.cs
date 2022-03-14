eec9eb6234e653aadc6267cb8dcec4378d2ce7f3
kiran1902 <kiru2kumar@gmail.com>
2022-03-14 22:34:08 +0530

    Fibonacci Series

--- LogicalProg/LogicalProg/Program.cs
+++ LogicalProg/LogicalProg/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to day 6 Logical Programs");
+﻿namespace LogicalProg
+{
+    class Fibonacci
+    {
+        static void Main(String[] args)
+        {
+            //local variables
+            int a = 0;
+            int b = 1;
+            int c;
+
+            Console.WriteLine("Enter The Number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.Write(a + " " + b + " "); //printing 0 and 1
+
+            //for loop
+            for (int i = 0; i < number; i++)
+            {
+                c = a + b;
+                Console.WriteLine(c);
+
+                //swapping a and b
+                a = b;
+                b = c;
+            }
+            Console.ReadLine();
+        }
+    }
+}
