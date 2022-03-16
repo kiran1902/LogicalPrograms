fb78cea048d6471c8cfca5165b9fecfcc06dc3a1
kiran1902 <kiru2kumar@gmail.com>
2022-03-16 12:57:18 +0530

    Coupon Number

--- LogicalProg/LogicalProg/Prog.cs
+++ LogicalProg/LogicalProg/Prog.cs
@@ -11,15 +11,21 @@ namespace LogicalProg
     {
         public void Test()
         {
+            //user input
             Console.WriteLine("Enter the number of Five Digits : ");
             int n = int.Parse(Console.ReadLine());
+
+            //creating an array to store
             int[] arrOfCoupons = new int[n];
 
+            //Local variable
             int countOfLoops=0,
                 numOfCoupons = 0;
 
+            //looping statement
             while(numOfCoupons < n)
             {
+                //to generate random numbers
                 Random random = new Random();
                 int result = random.Next(10000 , 100000);
 
@@ -27,7 +33,7 @@ namespace LogicalProg
                 {
                     numOfCoupons++;
                     Console.WriteLine("Result Does not Exist");
-                    n = arrOfCoupons[result];
+                    n = arrOfCoupons[result];  //storing the result in array
                 }
                 countOfLoops++;
             }
